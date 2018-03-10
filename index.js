4a62769ef5f0a81cfa6cb9d906596b606616a883
KeeganBruer <keegan.bruer@gmail.com>
2018-03-09 20:20:56 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var Discord = require("discord.js");
 let client = new Discord.Client();
 
 let mechanism = function mechanism() {
-    this.name = "default";
+    this.name = "This mechanism does not have a name.";
+    this.description = "This mechanism does not have a description.";
     this.votes = 0;
 };
 
@@ -14,6 +15,9 @@ let mechanisms = {
     },
     add: function(newMech) {
         this.array.push(newMech);
+    },
+    remove: function(i) {
+        this.array.splice(i, 1);
     },
     get: function(i) {
         return this.array[i];
@@ -56,29 +60,116 @@ let mechanisms = {
         
         this.array = quickSort(this.array, 0, this.array.length-1);
         
+    },
+    loadFromFile: function(fileName) {
+        
     }
 };
-
+let previousCommands = [];
 client.on("message", function(message) {
     mechanisms.sort();
     let messageArray = message.content.split(" ");
     let command = messageArray[0];
     if (command === "/add") {
+        previousCommands.unshift(messageArray);
+        let name = "";
+        let description = "";
+        let state = -1;
+        for (let i = 1; i < messageArray.length; i++) {
+            if (messageArray[i] === "name:") {
+                state = 0;
+            } else if (messageArray[i] === "description:") {
+                state = 1;
+            } else {
+                if (state === 0) {
+                    name += messageArray[i] + " ";
+                } else if (state === 1) {
+                    description += messageArray[i] + " ";
+                }
+            }
+        }
         let newMech = new mechanism();
-        newMech.name = messageArray[1];
+        if (name != "") {
+            newMech.name = name.trim();
+        }
+        if (description != "") {
+            newMech.description
+        }
         mechanisms.add(newMech);
     } else
     if (command === "/showList") {
-        let rtn = "\n";
+        previousCommands.unshift(messageArray);
+        let rtn = "\n ``` \n";
         for (let i = 0; i < mechanisms.size(); i++) {
             rtn += (i+1) + ") " + mechanisms.get(i).name + "\n";
         }
+        rtn += "```";
         message.reply(rtn);
     } else
-    if (command === "/vote") {
-        let number = messageArray[1];
-        mechanisms.get(number).votes += 1;
-        message.reply(mechanisms.get(number).name + " now has " + mechanisms.get(number).votes + " votes.");
+    if (command === "/showFullList") {
+        previousCommands.unshift(messageArray);
+        let rtn = "\n ``` \n";
+        for (let i = 0; i < mechanisms.size(); i++) {
+            rtn += (i+1) + ") " + mechanisms.get(i).name + "\n";
+            rtn += "\t Description: " + mechanisms.get(i).description + "\n";
+            rtn += "\t Votes: " + mechanisms.get(i).votes + "\n";
+        }
+        rtn += "```";
+        message.reply(rtn);
+    } else
+    if (command === "/upVote") {
+        previousCommands.unshift(messageArray);
+        let name = messageArray[1];
+        let number = null;
+        for (let i = 0; i < mechanisms.size(); i++) {
+            if (name === mechanisms.get(i).name) {
+                number = i;
+            }
+        }
+        if (number != null) {
+            mechanisms.get(number).votes += 1;
+            if (mechanisms.get(number).votes > 1) {
+                message.reply(mechanisms.get(number).name + " now has " + mechanisms.get(number).votes + " votes.");
+            } else {
+                 message.reply(mechanisms.get(number).name + " now has a vote.");
+            }
+        } else {
+            message.reply("No Mechanism By That Name.");
+        }
+    } else
+    if (command === "/downVote") {
+        previousCommands.unshift(messageArray);
+        let name = messageArray[1];
+        let number = null;
+        for (let i = 0; i < mechanisms.size(); i++) {
+            if (name === mechanisms.get(i).name) {
+                number = i;
+            }
+        }
+        if (number != null) {
+            if (mechanisms.get(number).votes > 0) {
+                mechanisms.get(number).votes -= 1;
+                if (mechanisms.get(number).votes > 1) {
+                    message.reply(mechanisms.get(number).name + " now has " + mechanisms.get(number).votes + " votes.");
+                } else {
+                     message.reply(mechanisms.get(number).name + " now has a vote.");
+                }
+            } else {
+                message.reply(mechanisms.get(number).name + " already has 0 votes.");
+            }
+        } else {
+            message.reply("No Mechanism By That Name.");
+        }
+    }
+    if (command === "/remove" || (previousCommands[0][0] === "/remove" && command === "yes")) {
+        previousCommands.unshift(messageArray);
+        if (command === "yes") {
+            let mechName = mechanisms.get(previousCommands[0][1]-1).name;
+            mechanisms.remove(previousCommands[0][1]-1);
+            message.reply(mechName + " has been removed")
+        } else {
+            message.reply("Are You Sure You Want To Remove " + mechanisms.get(messageArray[1]-1).name + "?");
+        }
     }
 });
 
